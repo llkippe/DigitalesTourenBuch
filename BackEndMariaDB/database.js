818804640609829f11703e8432265b35049f45d7
llkippe <102752683+llkippe@users.noreply.github.com>
2023-03-14 19:00:23 +0100

    debug

--- BackEndMariaDB/database.js
+++ BackEndMariaDB/database.js
@@ -2,8 +2,8 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
     host: 'localhost',
-    user: 'luca', //root
-    password: 'lucaPasswort', //maHmN!Lu!bD2004
+    user: 'root', //root   ||   luca
+    password: 'maHmN!Lu!bD2004', //maHmN!Lu!bD2004 ||| lucaPasswort
     database: 'touren'
 });
 
@@ -11,6 +11,7 @@ async function getSkitouren() {
     try{
         let con = await pool.getConnection();
         let res = await con.query("SELECT * FROM skitouren");
+        console.log(res);
         return res;
     }catch (error){
         console.log(error);
@@ -21,6 +22,7 @@ async function getSkitour(id) {
     try{
         let con = await pool.getConnection();
         let res = await con.query(`SELECT * FROM skitouren WHERE id = ${id}`);
+        console.log(res);
         return res;
     }catch (error){
         console.log(error);
@@ -31,6 +33,7 @@ async function insertSkitour(skitour) {
     try{
         let con = await pool.getConnection();
         let res = await con.query(`INSERT INTO skitouren VALUES(${skitour.Id},'${skitour.Bergname}',${skitour.Berghoehe},'${skitour.Datum}','${skitour.Personen}','${skitour.Beschreibung}')`);
+        console.log(res);
         return res;
     }catch (error){
         console.log(error);
