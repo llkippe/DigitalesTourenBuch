34c98003d5e1a2684feb9fddc5c272bc4e2d1941
llkippe <102752683+llkippe@users.noreply.github.com>
2023-03-14 14:57:47 +0100

    express update

--- BackEndSQL/app.js
+++ BackEndSQL/app.js
@@ -1,33 +1,38 @@
-const dbFunctions = require('./dbFunctions');
+// const dbFunctions = require('./dbFunctions');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-app.get('/api/touren/skitouren', (req, res) => {
-    dbFunctions.getSkitouren().then((product) => {
-        res.send(product[0]);
-    })
+app.get('/api/waslos', (req, res) => {
+    
+    res.send("halo");
 })
 
-app.get('/api/touren/skitouren/:id', (req, res) => {
-    dbFunctions.getSkitour(req.params.id).then((product) => {
-        res.send(product[0]);
-    })
-})
+// app.use(express.json());
 
-app.put('/api/touren/skitouren/', (req, res) => {
-    const skitour = {
-        Id: req.body.Id,
-        Bergname: req.body.Bergname,
-        Berghoehe: req.body.Berghoehe,
-        Datum: req.body.Datum,
-        Personen: req.body.Personen,
-        Beschreibung: req.body.Beschreibung
-    } 
-    dbFunctions.insertSkitour(skitour);
-    res.send(skitour);
-})
+// app.get('/api/touren/skitouren', (req, res) => {
+//     dbFunctions.getSkitouren().then((product) => {
+//         res.send(product[0]);
+//      })
+// })
+
+// app.get('/api/touren/skitouren/:id', (req, res) => {
+//     dbFunctions.getSkitour(req.params.id).then((product) => {
+//         res.send(product[0]);
+//     })
+// })
+
+// app.put('/api/touren/skitouren/', (req, res) => {
+//     const skitour = {
+//         Id: req.body.Id,
+//         Bergname: req.body.Bergname,
+//         Berghoehe: req.body.Berghoehe,
+//         Datum: req.body.Datum,
+//         Personen: req.body.Personen,
+//         Beschreibung: req.body.Beschreibung
+//     } 
+//     dbFunctions.insertSkitour(skitour);
+//     res.send(skitour);
+// })
 
 
 
