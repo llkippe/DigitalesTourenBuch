690f73c3b24725ce5dbb3b59c4efa30cbb960141
llkippe <102752683+llkippe@users.noreply.github.com>
2023-03-14 19:00:40 +0100

    debug

--- BackEndMariaDB/database.js
+++ BackEndMariaDB/database.js
@@ -2,8 +2,8 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
     host: 'localhost',
-    user: 'root', //root   ||   luca
-    password: 'maHmN!Lu!bD2004', //maHmN!Lu!bD2004 ||| lucaPasswort
+    user: 'luca', //root   ||   luca
+    password: 'lucaPasswort', //maHmN!Lu!bD2004 ||| lucaPasswort
     database: 'touren'
 });
 
